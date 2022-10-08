23649c047b7a0616d767f000fb945412f479251d
paulo.trindade <paulo.trindade@cartesianengenharia.com>
2022-10-08 18:00:52 -0300

    feat: Date and time ok

--- src/main.cpp
+++ src/main.cpp
@@ -7,61 +7,63 @@
 #include <RtcDS1302.h>
 #include "MAX30105.h"
 #include "heartRate.h"
-
+#include "SPIFFS.h"
+#include "FS.h"
 //------------------------------------Definições de rede-----------------------------
-#define WIFISSID "trator"                                    // Coloque seu SSID de WiFi aqui
-#define PASSWORD "Pauloh01"                                  // Coloque seu password de WiFi aqui
-#define TOKEN "BBFF-rX9QpMCCnyo6oNUW0xErfP0e7ixvjJ"          // Coloque seu TOKEN do Ubidots aqui
-#define variable_label_spo2 "Saturação de Oxigenio"          // Label referente a variável de temperatura criada no ubidots
-#define VARIABLE_LABEL_HEAT_RATE "Frequencia Cardiaca"       // Label referente a variável de umidade criada no ubidots
-#define variable_dia       "Dia"
-#define variable_mes       "Mês"
-#define variable_ano       "Ano"
-#define variable_hora      "Hora"
-#define variable_minuto    "Minuto"
-#define variable_segundos  "Segundos"
-#define DEVICE_ID "BBFF-fc2c3213408ddcd65448b2fc1996251f155" // ID do dispositivo (Device id, também chamado de client name)
-#define SERVER "things.ubidots.com"                          // Servidor do Ubidots (broker)
+#define WIFISSID                 "trator"                                    // Coloque seu SSID de WiFi aqui
+#define PASSWORD                 "Pauloh01"                                  // Coloque seu password de WiFi aqui
+#define TOKEN                    "BBFF-rX9QpMCCnyo6oNUW0xErfP0e7ixvjJ"       // Coloque seu TOKEN do Ubidots aqui
+#define VARIABLE_LABEL_SPO2      "Saturação de Oxigenio"                     // Label referente a variável de temperatura criada no ubidots
+#define VARIABLE_LABEL_HEAT_RATE "Frequencia Cardiaca"                       // Label referente a variável de umidade criada no ubidots
+#define VARIABLE_TEXT            "TITULO A SER DEFINIDO"
+#define variable_dia             "Dia"
+#define variable_mes             "Mês"
+#define variable_ano             "Ano"
+#define variable_hora            "Hora"
+#define variable_minuto          "Minuto"
+#define variable_segundos        "Segundos"
+#define DEVICE_ID                "BBFF-fc2c3213408ddcd65448b2fc1996251f155"    // ID do dispositivo (Device id, também chamado de client name)
+#define SERVER                   "things.ubidots.com"                          // Servidor do Ubidots (broker)
 #define PORT 1883                                            // Porta padrão
 
 // Tópico aonde serão feitos os publish, "esp32-dht" é o DEVICE_LABEL
 #define TOPIC "/v1.6/devices/CPAP_RESPIRE"
 int CLK = 5, RST = 12, DAT = 14;
 
-WiFiClient ubidots;           // Objeto WiFiClient usado para a conexão wifi
-PubSubClient client(ubidots); // Objeto PubSubClient usado para publish–subscribe
-WiFiServer sv(80);
-MAX30105 particleSensor;
-ThreeWire myWire(DAT,CLK,RST); // IO, SCLK, CE
+WiFiClient           ubidots;           // Objeto WiFiClient usado para a conexão wifi
+PubSubClient         client(ubidots); // Objeto PubSubClient usado para publish–subscribe
+WiFiServer           sv(80);
+MAX30105             particleSensor;
+ThreeWire            myWire(DAT,CLK,RST); // IO, SCLK, CE
 RtcDS1302<ThreeWire> Rtc(myWire);
-QueueHandle_t fila;
+QueueHandle_t        fila;
 
 struct
 {
-  double bpm;
-  double espo2;
-  double hora;
-  double minuto;
-  double segundos;
-  double dia;
-  double mes;
-  double ano;
+  float bpm;
+  float espo2;
+  int   hora;
+  int   minuto;
+  int   segundos;
+  int   dia;
+  int   mes;
+  int   ano;
   } typedef mqtt_dados_t;
 
-#define TIMETOBOOT 3000 // wait for this time(msec) to output SpO2
-#define SCALE 93.0      // 88.0      //adjust to display heart beat and SpO2 in the same scale
-#define SAMPLING 5      // if you want to see heart beat more precisely , set SAMPLING to 1
-#define FINGER_ON 30000 // if red signal is lower than this , it indicates your finger is not on the sensor
+#define TIMETOBOOT   3000       // wait for this time(msec) to output SpO2
+#define SCALE        93.0            // 88.0      //adjust to display heart beat and SpO2 in the same scale
+#define SAMPLING     5            // if you want to see heart beat more precisely , set SAMPLING to 1
+#define FINGER_ON    30000       // if red signal is lower than this , it indicates your finger is not on the sensor
 #define MINIMUM_SPO2 10.0
 
-const byte RATE_SIZE = 4; // Increase this for more averaging. 4 is good.
-byte rates[RATE_SIZE];    // Array of heart rates
-byte rateSpot = 0;
-long lastBeat = 0; // Time at which the last beat occurred
+const byte RATE_SIZE  = 4;     // Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE];        // Array of heart rates
+byte rateSpot         = 0;
+long lastBeat         = 0;            // Time at which the last beat occurred
 float beatsPerMinute;
-double beatAvg;
+float beatAvg;
 /* ------------------------------- RTC 1302 -------------------------------------------
-CLK/SCLK --> D5 - IO27
+CLK/SCLK --> D5 - IO05
 DAT/IO   --> D4 - IO12
 RST/CE   --> D2 - IO14
 VCC      --> 3,3v 
@@ -70,7 +72,7 @@ GND      --> GND*/
 //------------------------Funções--------------------------------------------------
 void reconnect();
 bool mqttInit();
-bool sendValues(double bpm, double espo2);
+bool sendValues(float bpm, float espo2);
 void mqtt_task(void *pvt);
 void printDateTime(const RtcDateTime& dt);
 
@@ -127,7 +129,7 @@ void setup()
 
   //--------------------------------------------------------------------------------------------------------
 
-  fila = xQueueCreate(40, sizeof(mqtt_dados_t));
+  fila = xQueueCreate(10, sizeof(mqtt_dados_t));
 
   // Initialize sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST))
@@ -140,32 +142,32 @@ void setup()
 
   byte ledBrightness = 0x0A; // Options: 0=Off to 255=50mA
   byte sampleAverage = 8;    // Options: 1, 2, 4, 8, 16, 32
-  byte ledMode = 2;          // Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  int sampleRate = 400;      // Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 69;       // Options: 69, 118, 215, 411
-  int adcRange = 2048;       // Options: 2048, 4096, 8192, 16384
+  byte ledMode       = 2;    // Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int sampleRate     = 400;  // Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth     = 69;   // Options: 69, 118, 215, 411
+  int adcRange       = 2048; // Options: 2048, 4096, 8192, 16384
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); // Configure sensor with these settings
 
-  xTaskCreatePinnedToCore(mqtt_task, "MQTT_TASK", 2048, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(mqtt_task, "MQTT_TASK", 4096, NULL, 1, NULL, 1);
 }
 //------------------------------------------------------------------------------------
-int tempo_envio = 0;
+int tempo_envio    = 0;
 //char y   
 uint32_t ir, red;
 float red_forGraph = 0.0;
-float ir_forGraph = 0.0;
-double avered = 0;
-double aveir = 0;
-double sumirrms = 0;
-double sumredrms = 0;
-int i = 0;
-int Num = 100; // calculate SpO2 by this sampling interval
-double fred, fir;
-double SpO2 = 0;
-double ESpO2 = 95.0; // initial value of estimated SpO2
-double FSpO2 = 0.7;  // filter factor for estimated SpO2
-double frate = 0.95; // low pass filter for IR/red LED value to eliminate AC component
+float ir_forGraph  = 0.0;
+float avered       = 0.0;
+float aveir        = 0.0;
+float sumirrms     = 0.0;
+float sumredrms    = 0.0;
+int i              = 0;
+int Num            = 100; // calculate SpO2 by this sampling interval
+float fred, fir;
+float SpO2         = 0.0;
+float ESpO2        = 95.0; // initial value of estimated SpO2
+float FSpO2        = 0.7;  // filter factor for estimated SpO2
+float frate        = 0.95; // low pass filter for IR/red LED value to eliminate AC component
 
 void loop()
 {
@@ -177,8 +179,8 @@ void loop()
   if (checkForBeat(irValue) == true)
   {
 
-    long delta = millis() - lastBeat;
-    lastBeat = millis();
+    long delta  = millis() - lastBeat;
+    lastBeat    = millis();
     beatsPerMinute = 60 / (delta / 1000.0);
 
     if (beatsPerMinute < 255 && beatsPerMinute > 20)
@@ -190,7 +192,7 @@ void loop()
       beatAvg = 0;
       for (byte x = 0; x < RATE_SIZE; x++)
         beatAvg += rates[x];
-      beatAvg /= RATE_SIZE;
+        beatAvg /= RATE_SIZE;
     }
   }
 
@@ -203,16 +205,16 @@ void loop()
     red = particleSensor.getFIFOIR(); // why getFOFOIR output Red data by MAX30102 on MH-ET LIVE breakout board
     ir = particleSensor.getFIFORed(); // why getFIFORed output IR data by MAX30102 on MH-ET LIVE breakout board
     i++;
-    fred = (double)red;
-    fir = (double)ir;
-    avered = avered * frate + (double)red * (1.0 - frate); // average red level by low pass filter
-    aveir = aveir * frate + (double)ir * (1.0 - frate);    // average IR level by low pass filter
+    fred = (float)red;
+    fir = (float)ir;
+    avered = avered * frate + (float)red * (1.0 - frate); // average red level by low pass filter
+    aveir = aveir * frate + (float)ir * (1.0 - frate);    // average IR level by low pass filter
     sumredrms += (fred - avered) * (fred - avered);        // square sum of alternate component of red level
     sumirrms += (fir - aveir) * (fir - aveir);             // square sum of alternate component of IR level
 
     if ((i % Num) == 0)
     {
-      double R = (sqrt(sumredrms) / avered) / (sqrt(sumirrms) / aveir);
+      float R = (sqrt(sumredrms) / avered) / (sqrt(sumirrms) / aveir);
       SpO2 = -23.3 * (R - 0.4) + 100;
       ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2; // passa baixa
       sumredrms = 0.0;
@@ -300,23 +302,14 @@ void reconnect()
   }
 }
 
-bool sendValues(double bpm, double espo2,double mes){ //, double mes
+bool sendValues(float bpm, float espo2, int dia,int mes,int ano,int hora, int minuto,int segundo){ //, float mes
   char json[250];
-
- // // Atribui para a cadeia de caracteres "json" os valores referentes a temperatura e os envia para a variável do ubidots correspondente
- // sprintf(json, "{\"%s\":{\"value\":%02.f}}", variable_dia, mes);
- // Serial.printf("%s\n", json);
- // if (!client.publish(TOPIC, json))
- //   return false;
- // 
- // // Atribui para a cadeia de caracteres "json" os valores referentes a temperatura e os envia para a variável do ubidots correspondente
- // sprintf(json, "{\"%s\":{\"value\":%02.f}}", variable_dia, ano);
- // Serial.printf("%s\n", json);
- // if (!client.publish(TOPIC, json))
- //   return false;
-
-  // Atribui para a cadeia de caracteres "json" os valores referentes a temperatura e os envia para a variável do ubidots correspondente
-  sprintf(json, "{\"%s\":{\"value\":%02.02f//%02.02f}}", variable_label_spo2, espo2,mes);
+ // Atribui para a cadeia de caracteres "json" os valores referentes a temperatura e os envia para a variável do ubidots correspondente
+  sprintf(json, "{\"%s\":{\"value\":%02.02f ; %02d/%02d/%02d ; %02d:%02d:%02d}}", VARIABLE_TEXT, espo2,dia,mes,ano,hora,minuto,segundo);
+  Serial.printf("%s\n", json);
+  if (!client.publish(TOPIC, json))
+    return false;
+  sprintf(json, "{\"%s\":{\"value\":%02.02f}}", VARIABLE_LABEL_SPO2, espo2);
   Serial.printf("%s\n", json);
   if (!client.publish(TOPIC, json))
     return false;
@@ -343,7 +336,9 @@ void mqtt_task(void *pvt)
     reconnect();
     while (xQueueReceive(fila, &dados, 100 * portTICK_PERIOD_MS))
     {
-      sendValues(dados.bpm, dados.espo2, dados.mes); //, dados.mes, dados.ano
+      sendValues( dados.bpm, dados.espo2,
+                  dados.dia, dados.mes,   dados.ano,
+                  dados.hora,dados.minuto,dados.segundos); //, dados.mes, dados.ano
     }
   }
 }
